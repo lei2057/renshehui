a25ac64c81b632e202e3dd8536f21b54e7a6e6c1
lei2057 <757940497@qq.com>
2019-06-12 08:58:58 +0800

    ceshi

---
+++ src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+asdasdasdas啊实打实打算
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
